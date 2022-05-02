01b3591dcc1379bf817618d57e9e57aa7766e799
Musayev442 <musayev442@gmail.com>
2022-05-02 17:48:13 +0400

    new update

--- lib/screens/categories/categories.dart
+++ lib/screens/categories/categories.dart
@@ -1,65 +1,34 @@
 // ignore_for_file: deprecated_member_use
 
 import 'package:firstapp/models/jewelries.dart';
-import 'package:firstapp/screens/categories/items.dart';
+import 'package:firstapp/screens/categories/shop.dart';
 import 'package:flutter/material.dart';
 
 class Categories extends StatelessWidget {
   final categoriesList = ["diamond", "gold", "watch", "necklace"];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: categoriesList
-            .map(
-              (category) => Container(
-                child: RaisedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Items(category)),
-                    );
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(5),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.3),
-                              spreadRadius: 0.1,
-                              blurRadius: 0.1,
-                              offset: Offset(0, 1),
-                            )
-                          ]),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(right: 5),
-                                child: Icon(Icons.diamond_outlined),
-                              ),
-                              Text(
-                                category,
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ],
-                          ),
-                          Icon(Icons.arrow_forward_ios_outlined),
-                        ],
-                      ),
-                    ),
-                  ),
+    return ListView(
+      children: categoriesList
+          .map(
+            (item) => ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.amber,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                side: BorderSide(
+                  color: Colors.black,
                 ),
               ),
-            )
-            .toList(),
-      ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Shop()),
+                );
+              },
+              child: Text(item),
+            ),
+          )
+          .toList(),
     );
   }
 }
---
+++ lib/screens/categories/shop.dart
@@ -0,0 +1,86 @@
+import 'package:firstapp/models/jewelries.dart';
+import 'package:firstapp/models/product_service.dart';
+import 'package:flutter/material.dart';
+
+import '../../models/data.dart';
+
+class Shop extends StatefulWidget {
+  @override
+  State<Shop> createState() => _ShopState();
+}
+
+class _ShopState extends State<Shop> {
+  var products;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    products = new ProductService().getProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Category"),
+      ),
+      body: FutureBuilder(
+        future: products,
+        builder: (BuildContext context, AsyncSnapshot<List<Data>> snapshot) {
+          if (snapshot.hasData) {
+            return ShopGrid(products.data);
+          }
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
+
+//StatefulWidget Cevirmek lazimdir----->Yeni Metodikaya Kec
+class ShopGrid extends StatelessWidget {
+  var data;
+  ShopGrid(this.data);
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      primary: false,
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 10,
+      mainAxisSpacing: 10,
+      crossAxisCount: 2,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text("He'd have you all unravel at the"),
+          color: Colors.teal[100],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Heed not the rabble'),
+          color: Colors.teal[200],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Sound of screams but the'),
+          color: Colors.teal[300],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Who scream'),
+          color: Colors.teal[400],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Revolution is coming...'),
+          color: Colors.teal[500],
+        ),
+        Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('Revolution, they...'),
+          color: Colors.teal[600],
+        ),
+      ],
+    );
+  }
+}
